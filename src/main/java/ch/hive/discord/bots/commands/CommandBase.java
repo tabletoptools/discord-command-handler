d541e8d3cdbf509acc90009b083ba94dca256d05
Carlo Field <cfi@bluesky-it.ch>
2017-08-16 18:16:21 +0200

    Project sources fetched from announcer bot

---
+++ src/main/java/ch/hive/discord/bots/commands/CommandBase.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright Carlo Field (cfi@bluesky-it.ch)
+ */
+package ch.hive.discord.bots.commands;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.MessageEmbed.Field;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+/**
+ *
+ * @author kileraptor1
+ */
+public class CommandBase {
+    private static CommandBase instance;
+    private Collection<Method> methods;
+    private String prefix = "";
+    
+    private CommandBase() {
+        methods = new ArrayList<>();
+    }
+    
+    public static CommandBase instance() {
+        if(instance == null) {
+            instance = new CommandBase();
+        }
+        return instance;
+    }
+    
+    public CommandBase registerCommandClass(Class<?> klass) {
+            while(klass != Object.class) {
+                final List<Method> allMethods = new ArrayList<>(Arrays.asList(klass.getDeclaredMethods()));
+                for(final Method method : allMethods) {
+                    if(method.isAnnotationPresent(Command.class)) {
+                        this.methods.add(method);
+                    }
+                }
+                klass = klass.getSuperclass();
+            }
+        return this;
+    }
+    
+    public CommandBase setPrefix(String prefix) {
+        this.prefix = prefix;
+        return this;
+    }
+    
+    public void process(MessageReceivedEvent event) {
+        if(!event.getMessage().getRawContent().startsWith(this.prefix)) return;
+        
+        String[] splitMessage = event.getMessage().getRawContent().split(" ");
+        String command = splitMessage[0].substring(this.prefix.length()).toLowerCase();
+        if(command.toLowerCase().equals("help".toLowerCase()))
+            help(event);
+        for (Method method : this.methods) {
+            if(method.getAnnotation(Command.class).value().toLowerCase().equals(command)) {
+                try {
+                    Class<?>[] paramTypes = method.getParameterTypes();
+                    if(splitMessage.length != paramTypes.length) return;
+                    List<Object> objects = new ArrayList<>();
+                    objects.add(event);
+                    int x = 1;
+                    while(x < paramTypes.length) {
+                        objects.add(paramTypes[x].cast(splitMessage[x]));
+                        x++;
+                    }
+                    method.invoke(null, objects.toArray(new Object[objects.size()]));
+                }
+                catch(IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                    event.getChannel().sendMessage("Syntax error.").queue();
+                }
+                return;
+            }
+        }
+    }
+    private void help(MessageReceivedEvent event) {
+        int fieldCount = 0;
+        final int maxCount = 10;
+        EmbedBuilder builder = new EmbedBuilder();
+        for(Method method : this.methods) {
+            Field field;
+            String title = this.prefix 
+                    + method.getAnnotation(Command.class).value()
+                    + getParamsAsString(method);
+            if(method.isAnnotationPresent(Description.class)) {
+                
+                field = new Field(title, 
+                        method.getAnnotation(Description.class).value(), false);
+            }
+            else {
+                field = new Field(title, "", false);
+            }
+            builder.addField(field);
+            
+            fieldCount++;
+            if(fieldCount == maxCount) {
+                fieldCount = 0;
+                event.getChannel().sendMessage(builder.build()).queue();
+                builder.clearFields();
+            }
+        }
+        if(!builder.getFields().isEmpty()) {
+            event.getChannel().sendMessage(builder.build()).queue();
+        }
+    }
+    
+    private String getParamsAsString(Method method) {
+        StringBuilder paramstring = new StringBuilder();
+        java.lang.reflect.Parameter[] params = method.getParameters();
+        if(params.length == 1) return paramstring.toString();
+        for(int x = 1; x < params.length; x++) {
+            paramstring.append(" ");
+            if(params[x].isAnnotationPresent(Parameter.class)) {
+                paramstring.append('[');
+                paramstring.append(params[x].getAnnotation(Parameter.class).value());
+                paramstring.append(']');
+            }
+            else {
+                paramstring.append('[');
+                paramstring.append(params[x].getType().getName());
+                paramstring.append(' ');
+                paramstring.append(params[x].getName());
+                paramstring.append(']');
+            }
+        }
+        
+        return paramstring.toString();
+    }
+    
+}
---
+++ src/main/java/ch/hive/discord/bots/commands/Parameter.java
@@ -0,0 +1,27 @@
+/*
+ * $Id: ParamDescriptor 3988 2017-06-21 13:47:09Z cfi $
+ * Created on Aug 10, 2017, 5:20:50 PM
+ * 
+ * Copyright (c) 2017 by bluesky IT-Solutions AG,
+ * Kaspar-Pfeiffer-Strasse 4, 4142 Muenchenstein, Switzerland.
+ * All rights reserved.
+ * 
+ * This software is the confidential and proprietary information
+ * of bluesky IT-Solutions AG ("Confidential Information").  You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with bluesky IT-Solutions AG.
+ */
+package ch.hive.discord.bots.commands;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ *
+ * @author cfi
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Parameter {
+    String value();
+}
